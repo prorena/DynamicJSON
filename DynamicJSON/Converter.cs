57753f39a36409b82e118cc7557dbaa93634b317
Florian Weihmann <opensource@prorena.de>
2024-07-16 10:08:14 +0200

    init

---
+++ DynamicJSON/Converter.cs
@@ -0,0 +1,195 @@
+﻿///////////////////////////////////////
+// Part of Prorena dynamic JSON; an  //
+// open-source converter for JObjects//
+// into real dynamic ExpandoObjects. //
+//                                   //
+// Copyright © 2024 by Prorena GmbH. //
+// Author: Florian Weihmann          //
+// Licensed under GPL v.3            //
+///////////////////////////////////////
+
+using Newtonsoft.Json.Linq;
+using System.Dynamic;
+using System.Text.Json.Nodes;
+
+namespace DynamicJSON
+{
+    /// <summary>
+    /// Converts JSONs into dynamic ExpandoObjects
+    /// </summary>
+    public static class Converter
+    {
+        /// <summary>
+        /// Converts a string containing a JSON into a dynamic ExpandoObject
+        /// </summary>
+        /// <param name="json">The input JSON-string</param>
+        /// <returns>A dynamic ExpandoObject representing the JSON strcture</returns>
+        public static dynamic? ToDynamicJson(this string json)
+        {
+            return JObject.Parse(json).ToDynamic();
+        }
+
+#if NET8_0
+        /// <summary>
+        /// Converts JsonNode into a dynamic ExpandoObject
+        /// </summary>
+        /// <param name="json">The input JSON</param>
+        /// <returns>A dynamic ExpandoObject representing the JSON strcture</returns>
+        /// <see cref="System.Text.Json.Nodes.JsonNode"/>
+        /// <see cref="System.Text.Json.Nodes.JsonObject"/>
+        /// <see cref="System.Text.Json.Nodes.JsonValue"/>
+        public static dynamic? ToDynamic(this JsonNode? json)
+        {
+            if (json == null)
+                return null;
+
+            if (json.GetType() == typeof(JsonArray))
+            {
+                var output = new List<object>();
+                foreach (var item in json as JsonArray)
+                    output.Add(item.ToDynamic());
+                return output;
+            }
+            else if (json.GetType() == typeof(JsonObject))
+            {
+                var output = new ExpandoObject() as IDictionary<string, object?>;
+                foreach (var item in json as JsonObject)
+                    output.Add(item.Key, item.Value.ToDynamic());
+                return output;
+            }
+            else
+                switch (json.GetValueKind())
+                {
+                    case System.Text.Json.JsonValueKind.Number:
+                        try { return json.GetValue<byte>(); } catch (Exception) { }
+                        try { return json.GetValue<int>(); } catch (Exception) { }
+                        return json.GetValue<double>();
+
+                    case System.Text.Json.JsonValueKind.True:
+                        return true;
+                    case System.Text.Json.JsonValueKind.False:
+                        return false;
+                    case System.Text.Json.JsonValueKind.Undefined:
+                    case System.Text.Json.JsonValueKind.Null:
+                        return null;
+                    default:
+                    case System.Text.Json.JsonValueKind.String:
+                        return json.ToString();
+
+                }
+        }
+#endif
+
+        /// <summary>
+        /// Converts JObject / JToken into a dynamic ExpandoObject
+        /// </summary>
+        /// <param name="json">The input JSON</param>
+        /// <returns>A dynamic ExpandoObject representing the JSON strcture</returns>
+        /// <see cref="Newtonsoft.Json.Linq.JToken"/>
+        /// <see cref="Newtonsoft.Json.Linq.JObject"/>
+        /// <see cref="Newtonsoft.Json.Linq.JProperty"/>
+        public static dynamic? ToDynamic(this JToken? json)
+        {
+            if (json == null)
+                return null;
+
+            switch (json.Type)
+            {
+                case JTokenType.Object:
+                    {
+                        var output = new ExpandoObject() as IDictionary<string, object?>;
+                        foreach (var item in (JObject)json)
+                            output.Add(item.Key, item.Value.ToDynamic());
+                        return output;
+                    }
+
+                case JTokenType.Array:
+                    {
+                        var output = new List<object>();
+                        foreach (var item in json)
+                            output.Add(item.ToDynamic());
+                        return output;
+                    }
+
+                case JTokenType.Constructor:
+                    {
+                        var jConstructor = (JConstructor)json;
+                        switch (jConstructor.Count)
+                        {
+                            case 0:
+                                return null;
+
+                            case 1:
+                                return jConstructor.First.ToDynamic();
+
+                            default:
+                                if (jConstructor.First!.Type == JTokenType.String)
+                                {
+                                    var result = new ExpandoObject() as IDictionary<string, object?>;
+
+                                    if (jConstructor.Count == 2)
+                                        result.Add((string)jConstructor.First!, jConstructor.Last.ToDynamic());
+                                    else
+                                    {
+                                        result.Add((string)jConstructor.First!, new List<object>());
+
+                                        foreach (var item in jConstructor.Skip(1))
+                                            ((List<object>)result[(string)jConstructor.First!]!).Add(item.ToDynamic());
+                                    }
+                                    return result;
+                                }
+                                else
+                                {
+                                    var output = new List<object>();
+                                    foreach (var item in json)
+                                        output.Add(item.ToDynamic());
+                                    return output;
+                                }
+                        }
+                    }
+
+                case JTokenType.Property:
+                    {
+                        var result = new ExpandoObject() as IDictionary<string, object?>;
+                        result.Add(((JProperty)json).Name, ((JProperty)json).Value.ToDynamic());
+                        return result;
+                    }
+                case JTokenType.Integer:
+                    return (int)json;
+                case JTokenType.Float:
+                    return (double)json;
+                case JTokenType.Boolean:
+                    return (bool)json;
+
+                case JTokenType.None:
+                case JTokenType.Null:
+                case JTokenType.Undefined:
+                case JTokenType.Comment:
+                    return null;
+
+                case JTokenType.Date:
+                    return (DateTime)json;
+                case JTokenType.Raw:
+                    {
+                        var result = (JToken?)((JRaw)json!)!.Value;
+                        if (result == null)
+                            return null;
+
+                        return result.ToDynamic();
+                    }
+                case JTokenType.Bytes:
+                    return (byte[]?)json;
+                case JTokenType.Guid:
+                    return new Guid(json.ToString());
+                case JTokenType.Uri:
+                    return new Uri(json.ToString());
+                case JTokenType.TimeSpan:
+                    return (TimeSpan)json;
+
+                case JTokenType.String:
+                default:
+                    return json.ToString();
+            }
+        }
+    }
+}
---
+++ DynamicJSONTests/ConverterTests.cs
@@ -0,0 +1,127 @@
+﻿///////////////////////////////////////
+// Part of Prorena dynamic JSON; an  //
+// open-source converter for JObjects//
+// into real dynamic ExpandoObjects. //
+//                                   //
+// Copyright © 2024 by Prorena GmbH. //
+// Author: Florian Weihmann          //
+// Licensed under GPL v.3            //
+///////////////////////////////////////
+
+using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+
+namespace DynamicJSON.Tests
+{
+    [TestClass()]
+    public class ConverterTests
+    {
+        [TestMethod()]
+        public void ToDynamicJsonTest()
+        {
+            var json = "{\"name\":\"Max\",\"alter\":40,\"anschrift\":{\"strasse\":\"Waldweg\",\"nr\":4.8},\"single\":true,\"pets\":null,\"books\":[\"Harry Potter I\", \"Harry Potter II\"]}";
+            var dyn = json.ToDynamicJson();
+            Assert.IsNotNull(dyn);
+            Assert.AreEqual("Max", dyn!.name);
+            Assert.AreEqual(40, dyn.alter);
+            Assert.AreEqual("Waldweg", dyn.anschrift.strasse);
+            Assert.AreEqual(4.8, dyn.anschrift.nr);
+            Assert.AreEqual(true, dyn.single);
+            Assert.IsNull(dyn.pets);
+            Assert.IsInstanceOfType(dyn.books, typeof(List<object>));
+            Assert.AreEqual(2, ((List<object>)dyn.books).Count);
+            Assert.AreEqual("Harry Potter I", dyn.books[0]);
+        }
+
+        [TestMethod()]
+        public void ToDynamicTest()
+        {
+            JObject? j = null;
+            Assert.IsNull(j.ToDynamic());
+        }
+
+        [TestMethod()]
+        public void SpecialTest()
+        {
+            var json = new JObject
+            {
+                new JProperty("ts", TimeSpan.FromDays(5)),
+                new JProperty("guid", new Guid()),
+                new JProperty("uri", new Uri("https://prorena.de/")),
+                new JProperty("date", DateTime.Now),
+                new JProperty("bytes",new byte[] {9,6, 3 }),
+            };
+            var dyn = json.ToDynamic();
+
+            Assert.IsNotNull(dyn);
+            Assert.IsInstanceOfType<TimeSpan>(dyn!.ts);
+            Assert.IsInstanceOfType<Guid>(dyn.guid);
+            Assert.IsInstanceOfType<Uri>(dyn.uri);
+            Assert.IsInstanceOfType<DateTime>(dyn.date);
+            Assert.IsInstanceOfType<byte[]>(dyn.bytes);
+
+
+            Assert.IsNull(new JConstructor("test").ToDynamic());
+
+            Assert.AreEqual("test1", new JConstructor("name", "test1").ToDynamic());
+            dyn = new JConstructor("name", "test1", "test2").ToDynamic();
+            Assert.IsNotNull(dyn);
+            Assert.AreEqual("test2", dyn!.test1);
+
+            dyn = new JConstructor("name", 5, "test2").ToDynamic();
+            Assert.IsInstanceOfType<List<object>>(dyn);
+            Assert.AreEqual(5, dyn![0]);
+            Assert.AreEqual("test2", dyn[1]);
+
+#pragma warning disable CS8625
+            dyn = new JConstructor("name", null, "test2").ToDynamic();
+            Assert.IsInstanceOfType<List<object>>(dyn);
+            Assert.IsNull(dyn![0]);
+#pragma warning restore
+
+            dyn = new JConstructor("name", "test1", "test2", "test3").ToDynamic();
+            Assert.IsInstanceOfType<dynamic>(dyn);
+            Assert.AreEqual("test2", dyn!.test1[0]);
+            Assert.AreEqual("test3", dyn.test1[1]);
+
+            dyn = new JConstructor("ctorExample",
+                new JValue("value"),
+                new JObject(new JProperty("prop1", new JValue(1)), new JProperty("prop2", new JValue(2)))
+                ).ToDynamic();
+
+            Assert.AreEqual(1, dyn!.value.prop1);
+            Assert.AreEqual(2, dyn!.value.prop2);
+
+            dyn = new JProperty("text", "value").ToDynamic();
+            Assert.IsNotNull(dyn);
+            Assert.AreEqual("value", dyn!.text);
+
+            dyn = new JRaw(json).ToDynamic();
+            Assert.IsNotNull(dyn);
+            Assert.IsInstanceOfType<TimeSpan>(dyn!.ts);
+            Assert.AreEqual(TimeSpan.FromDays(5), dyn.ts);
+
+            Assert.IsNull(new JRaw((object?)null).ToDynamic());
+        }
+
+#if NET8_0
+        [TestMethod()]
+        public void MsTest()
+        {
+            var json = JsonObject.Parse("{\"name\":\"Max\",\"alter\":40,\"anschrift\":{\"strasse\":\"Waldweg\",\"nr\":4.8},\"single\":true,\"pets\":null,\"books\":[\"Harry Potter I\", \"Harry Potter II\"],\"happy\":false}");
+            var dyn = json.ToDynamic();
+            Assert.IsNotNull(dyn);
+            Assert.AreEqual("Max", dyn!.name);
+            Assert.AreEqual(40, dyn.alter);
+            Assert.AreEqual("Waldweg", dyn.anschrift.strasse);
+            Assert.AreEqual(4.8, dyn.anschrift.nr);
+            Assert.AreEqual(true, dyn.single);
+            Assert.AreEqual(false, dyn.happy);
+            Assert.IsNull(dyn.pets);
+            Assert.IsInstanceOfType(dyn.books, typeof(List<object>));
+            Assert.AreEqual(2, ((List<object>)dyn.books).Count);
+            Assert.AreEqual("Harry Potter I", dyn.books[0]);
+        }
+#endif
+    }
+}
